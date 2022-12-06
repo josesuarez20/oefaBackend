3e39d768e6f8cac7bc3da3b6a162df879f51f9e9
Jonathan Narvaez <jonaser06@gmail.com>
2022-12-06 00:02:21 -0500

    fix

--- sirte-canonical/src/main/java/pe/gob/oefa/dto/core/UserDTO.java
+++ sirte-canonical/src/main/java/pe/gob/oefa/dto/core/UserDTO.java
@@ -5,6 +5,16 @@ public class UserDTO implements Serializable {
 	private String codigousuario;
 	private String documento;
 	private String nombrecompleto;
+	private String codigopuesto;
+	private String descripcion;
+	private long nombre;
+	private String area;
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
 	public String getCodigousuario() {
 		return codigousuario;
 	}
@@ -17,11 +27,30 @@ public class UserDTO implements Serializable {
 	public void setDocumento(String documento) {
 		this.documento = documento;
 	}
+	
+	public long getNombre() {
+		return nombre;
+	}
+	public void setNombre(long nombre) {
+		this.nombre = nombre;
+	}
 	public String getNombrecompleto() {
 		return nombrecompleto;
 	}
 	public void setNombrecompleto(String nombrecompleto) {
 		this.nombrecompleto = nombrecompleto;
+	}
+	public String getCodigopuesto() {
+		return codigopuesto;
+	}
+	public void setCodigopuesto(String codigopuesto) {
+		this.codigopuesto = codigopuesto;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
 	}
 	
 }
